a9b45170cc4a212e1583cac04568609c7e9fb210
NexusSfan <nexussfan@duck.com>
2026-02-16 07:14:48 -0800

    AMDGPU AMD64 might not have recieved this update. For some reason reprepro is being annoying

--- include.sh
+++ include.sh
@@ -1,5 +1,7 @@
 reprepro -C stable includedeb main ~/XLibreDebianRemote/ubuntu-packages-amd64/*.deb
 reprepro -C stable includedeb main ~/XLibreDebianRemote/ubuntu-packages-arm64/*.deb
+reprepro -C stable includedeb main ~/Debian/Projects/xlibre-keyring.deb
+reprepro -C stable includedeb main ~/Debian/Projects/nexussfan-keyring.deb
 reprepro export
 reprepro deleteunreferenced
 reprepro export
